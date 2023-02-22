edb5d4da3796c516e66c86fd432faa20f6dc67ce
Taher M <taher.m@foundationai.com>
2023-02-22 20:28:26 +0530

    Fix cors

--- sse.py
+++ sse.py
@@ -3,8 +3,8 @@ import threading
 import time
 
 import redis
-from flask import Flask, render_template
-from flask_sse import sse
+from flask import Flask, render_template, request, current_app, stream_with_context
+from flask_sse import ServerSentEventsBlueprint
 from gunicorn.app.base import Application, BaseApplication
 
 app = Flask(__name__)
@@ -13,13 +13,35 @@ redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_port = os.getenv('REDIS_PORT', '6379')
 
 app.config["REDIS_URL"] = f"redis://{redis_host}:{redis_port}"
+
+
+class CustomBlueprint(ServerSentEventsBlueprint):
+    def stream(self):
+        channel = request.args.get('channel') or 'sse'
+
+        @stream_with_context
+        def generator():
+            for message in self.messages(channel=channel):
+                yield str(message)
+
+        return current_app.response_class(
+            generator(),
+            mimetype='text/event-stream',
+            headers={'Access-Control-Allow-Origin': '*'}
+        )
+
+
+sse = CustomBlueprint('sse', __name__)
+sse.add_url_rule(rule="", endpoint="stream", view_func=sse.stream)
+
 app.register_blueprint(sse, url_prefix='/stream')
 
 
 def start_listening():
     try:
         print('Start listening')
-        rd = redis.StrictRedis(redis_host, redis_port, charset="utf-8", decode_responses=True)
+        rd = redis.StrictRedis(redis_host, redis_port,
+                               charset="utf-8", decode_responses=True)
         sub = rd.pubsub()
         sub.subscribe('mq1')
         for message in sub.listen():
@@ -71,3 +93,4 @@ if __name__ == '__main__':
     t1.start()
 
     StandaloneApplication(app, options).run()
+    # app.run(port=5001)
